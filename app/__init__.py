af874e99001a6c881ee2b3b90226191fa68aeba2
Dhruva Barua <dbarua2023@gmail.com>
2025-10-28 04:07:59 -0400

    new changes for cart to work on http://127.0.0.1:5000/cart?user_id=1

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 
 from .config import Config
 from .db import DB
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__)
+
     app.config.from_object(Config)
+
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     app.db = DB(app)
 
@@ -18,5 +22,8 @@ def create_app():
 
     from .index import bp as index_bp
     app.register_blueprint(index_bp)
+
+    from .cart_routes import bp as cart_bp
+    app.register_blueprint(cart_bp)
 
     return app
