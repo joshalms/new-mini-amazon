35cca5ac3ec954795dacd561f584486b0abac573
Mauricio Hernandez-Gonzalezr <mauricio.hernandez-gonzalezr@duke.edu>
2025-12-11 07:36:21 -0500

    Product Testing

--- db/create.sql
+++ db/create.sql
@@ -43,7 +43,9 @@ create table products (
     name text unique not null,
     price decimal(12,2) not null,
     available boolean default true,
-    
+    image_url text,
+    description text
+
 );
 
 ALTER TABLE products ADD COLUMN image_url TEXT;
